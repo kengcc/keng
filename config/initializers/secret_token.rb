8c73c82281c576347e8176fb240e7185a40f2663
Chee-Chan Keng <kengcc@gmail.com>
2013-06-29 09:12:03 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Keng::Application.config.secret_key_base = 'ad7c6c74ccac680cae59fddda0265f1b5c3490e39b303fb34ebd60172ed76b5e1681005911284156e26dedcf1fa6e216c927d57d5d930391051232e4ba8171a4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Keng::Application.config.session_store :cookie_store, key: '_keng_session'
